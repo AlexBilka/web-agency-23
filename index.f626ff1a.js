1dfad8da10f534ac5b427f0a83f88b90628cf3ad
AlexBilka <114955573+AlexBilka@users.noreply.github.com>
2023-01-02 09:13:45 +0000

    Deploying to gh-pages from @ AlexBilka/web-agency-23@69c1a47b09fb8d8fdd41f492d28ee013ca5c83bc 🚀

---
+++ index.f626ff1a.js
@@ -0,0 +1,2 @@
+var scrollTopBtn=document.querySelector(".scroll-to-top");function scrollFunction(){document.body.scrollTop>250||document.documentElement.scrollTop>250?scrollTopBtn.style.display="flex":scrollTopBtn.style.display="none"}window.onscroll=function(){scrollFunction()};
+//# sourceMappingURL=index.f626ff1a.js.map
